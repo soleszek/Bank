dc88e331f39758152299d0364e73d8c25290c6a4
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-07-21 12:12:13 +0200

    added methods

--- src/Account.java
+++ src/Account.java
@@ -2,11 +2,51 @@ public abstract class Account {
 
     private String clientData;
     private int number;
-    private Boolean state;
+    private Boolean isOpen = false;
 
-    
+    public Account(String clientData, int number, Boolean state) {
+        this.clientData = clientData;
+        this.number = number;
+        this.isOpen = state;
+    }
 
-    public void openAccount(){};
-    public void closeAccount(){};
+    public void openAccount(){
 
+    }
+    public void closeAccount(){
+
+    }
+    public void payMoney(){};
+    public void takeMoney(){};
+
+    public enum accountTypes {
+        personal,
+        corporate;
+    }
+
+
+
+    public String getClientData() {
+        return clientData;
+    }
+
+    public void setClientData(String clientData) {
+        this.clientData = clientData;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Boolean getState() {
+        return isOpen;
+    }
+
+    public void setState(Boolean state) {
+        this.isOpen = state;
+    }
 }
